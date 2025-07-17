bc0bc217b28c22482e54bf321f9225aa808b1dbd
Guilherme Faccin <gfaccin27@gmail.com>
2025-07-17 11:50:31 -0300

    fix: page responsivitiness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import {TestimonialsSection} from '@/components/testimonials'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#181818] text-white pt-[76px]">
+    <div className="min-h-screen bg-[#181818] text-white pt-[76px] overflow-x-hidden">
       {" "}
       <Header />
       <HeroSection />
